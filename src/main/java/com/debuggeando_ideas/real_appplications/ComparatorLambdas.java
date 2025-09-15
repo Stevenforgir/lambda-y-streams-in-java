f3af73db930bd648f716d112b41f9dda1a4a4084
StevenUninorte <stevenforgir@gmail.com>
2025-09-15 16:22:43 -0500

    reading a file

--- src/main/java/com/debuggeando_ideas/real_appplications/ComparatorLambdas.java
+++ src/main/java/com/debuggeando_ideas/real_appplications/ComparatorLambdas.java
@@ -3,14 +3,18 @@ package com.debuggeando_ideas.real_appplications;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ComparatorLambdas {
 
-    public static void main(String[] args) {
-    }
+
 }
 
 @Data
